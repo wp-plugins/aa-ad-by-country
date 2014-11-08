809486ce6adc5693ad4b32a8a2276c14387ffdef
aaextention <aaextention@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-08 08:36:27 +0000

    released
    git-svn-id: https://plugins.svn.wordpress.org/aa-ad-by-country/trunk@1021899 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ aadbycountry.php
@@ -0,0 +1,63 @@
+<?php 
+/**
+ * Plugin Name: AA Ad by country
+ * Plugin URI: http://wordpress.org/.../
+ * Description: use like [AD for="BD"]Example[/AD]
+ * Version: 1.0
+ * Developer + Idea: A. Roy / A. Mahmud
+ * Author URI: http://webdesigncr3ator.com
+ * Support Email : contactus.aa@gmail.com
+ * License: GPL2
+ **/
+	
+	
+		
+function aa_ad_getUserIP()
+{
+    $client  = @$_SERVER['HTTP_CLIENT_IP'];
+    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+    $remote  = $_SERVER['REMOTE_ADDR'];
+
+    if(filter_var($client, FILTER_VALIDATE_IP))
+    {
+        $ip = $client;
+    }
+    elseif(filter_var($forward, FILTER_VALIDATE_IP))
+    {
+        $ip = $forward;
+    }
+    else
+    {
+        $ip = $remote;
+    }
+
+    return $ip;
+}
+
+
+
+
+function aa_ad_by_country($atts, $content = null){
+	
+    $a = shortcode_atts( array(
+		'for'=> ''
+    ), $atts );
+
+	
+
+
+	$json = file_get_contents('http://ip-api.com/json/'.aa_ad_getUserIP());
+	$obj = json_decode($json);
+		
+
+	//get user country	
+	$usr_country = $obj->countryCode;
+	
+	if($a['for']==$usr_country){
+		return $content;
+	}else{
+		return "You country not support this content";
+	}
+}
+	add_shortcode( 'AD', 'aa_ad_by_country' );
+	
---
+++ readme.txt
@@ -0,0 +1,11 @@
+=== Plugin Name ===
+AA Ad by country
+
+Tags:aa , aa extension,extension,ad by country , ip
+Requires at least:4.0
+Tested up to:4.0
+Stable tag:aaadbycountry
+License: GPL
+Author :A. Roy / A. Mahmud
+==Description==
+[AD for="BD"]sample content[/AD]
